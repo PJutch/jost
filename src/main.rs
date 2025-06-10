4c6937cebf69d456ef97601efddfe48661597d49
PJutch <103033969+PJutch@users.noreply.github.com>
2025-06-11 01:11:34 +0300

    Add simple loop that parses numbers and pushes them onto the stack

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::io;
+use std::vec::Vec;
+
+#[derive(Debug)]
+enum Value {
+    Int(i64)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let mut stack = Vec::<Value>::new();
+    for line in stdin.lines() {
+        let line = line.unwrap();
+        if let Ok(number) = line.parse::<i64>() {
+            stack.push(Value::Int(number));
+            println!("{stack:?}");
+        } else {
+            println!("Unknown word");
+        }
+    }
 }
